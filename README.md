77551b678604c2011b782efbc214467ed82b6fd2
Gawain Hewitt <gawain@gawainhewitt.co.uk>
2023-11-01 07:20:11 +0000

    refactor key input

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
-# codurance-workshop
-Resources for my workshop Thursday 2nd at LSCC
+# LSCC Workshop
 
-uses note names see https://newt.phys.unsw.edu.au/jw/notes.html
+Resources for a talk and workshop with Gawain Hewitt at LSCC
+
+https://gawainhewitt.co.uk
+
+https://github.com/gawainhewitt 
+
+We are using Tone.js for sound and P5.js for images today. 
+
+I have tried to keep this repo as simple as possible to keep things playful. 
+
+### Tone.js
+
+This is an excellent library and very well documented. The documentation does assume music and sound engineering knowledge however. Please do ask me if you want anything explained. 
+
+I have accessed the library with a CDN for this session to make this easy. You can see the link for this in the head of the html file. Normally I would install it using NPM.
+
+### Midi Note Names
+
+https://newt.phys.unsw.edu.au/jw/notes.html
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// variables for sound elements
+
 let duration = "8n"; // https://tonejs.github.io/docs/14.7.77/type/Time
 let sequencerSpeed = "8n";
 let synthVolume = -10;
@@ -7,13 +9,10 @@ let delayFeedback = 0.6;
 const notes = ["F3", "G#3", "C4", "D#4", "G4"];    // array containing our musical notes that we are currently using (tone.js will respond to these as is)
 let sequencerNote = 0;
 
-
+// set up user interface
 
 let volumeSlider = document.getElementById("volume");
 let cutoffSlider = document.getElementById("cutoff");
-
-const visualisationMeter = new Tone.Meter();
-visualisationMeter.normalRange = true;
 
 document.addEventListener('keydown', handleKeyDown); //add listener for keyboard input
 document.addEventListener('keyup', handleKeyUp); //add listener for keyboard input
@@ -28,17 +27,19 @@ cutoffSlider.oninput = function() {
     synth.filterEnvelope.baseFrequency = frequency;
 }
 
-const gainNode = new Tone.Gain().toDestination();
+// set up Tone.js
 
-gainNode.connect(visualisationMeter);
+const visualisationMeter = new Tone.Meter(); // this gives us the information to make a visualisation
+visualisationMeter.normalRange = true; // means the numbers will be between 0 and 1
+
+const gainNode = new Tone.Gain().toDestination(); // create and connect our gain node to the "destination" - i.e speakers / headphones
+
+gainNode.connect(visualisationMeter); // connect our gain node to the visualisation meter
 
 // set up the delay
-
 const feedbackDelay = new Tone.FeedbackDelay(delayTime, delayFeedback).connect(gainNode);
 
-
 // set up the synth
-
 const synth = new Tone.MonoSynth({
     oscillator: {
         type: "square"
@@ -70,7 +71,6 @@ const synth = new Tone.MonoSynth({
 }).connect(feedbackDelay)
 
 // set up the sampler
-
 const sampler = new Tone.Sampler({
 	urls: {
 		A1: "sounds/ruthLoop4.flac"
@@ -91,12 +91,12 @@ function stopSynth() {
 }
 
 function playSample(i = 0) {
-    sampler.triggerAttackRelease(notes[i]);
+    sampler.triggerAttackRelease(notes[i]); // plays a sample
 }
 
 function startSequence() {
-    Tone.Transport.start();
-    Tone.Transport.scheduleRepeat(repeat, sequencerSpeed);
+    Tone.Transport.start(); // starts the transport - this refers to a metaphor of a tape machine in a recording studio
+    Tone.Transport.scheduleRepeat(repeat, sequencerSpeed); // calls the function repeat() every sequencerSpeed interval
 }
 
 function repeat() {
@@ -105,91 +105,41 @@ function repeat() {
 }
 
 function stopSequence() {
-    Tone.Transport.stop();
-    synth.triggerRelease(); // stops a sound
+    Tone.Transport.stop(); // stops the transport
+    synth.triggerRelease(); // stops a sound - this is because sometimes we have a hanging note
 
 }
 
 
 // read Qwerty keyboard and play notes from it
+// the "debounce" is so it only calls playSynth once on a key press
 
-let keyDebouncing = [0,0,0,0,0];
+const keys = ["ArrowLeft", "ArrowRight", "ArrowDown", "ArrowUp", "Space"];
+let keyDebouncing = [false ,false ,false ,false ,false ];
 
 function handleKeyDown(e) {
-
-    var key = e.code;
-  
-    switch(key) {  
-      case "ArrowLeft" :
-        if(keyDebouncing[0] === 0) {
-          playSynth(0);
-          keyDebouncing[0] = 1;
-          break;
-        } else {
-          break;
-        }
-      case "ArrowRight" :
-        if(keyDebouncing[1] === 0) {
-          playSynth(1);
-          keyDebouncing[1] = 1;
-          break;
-        } else {
-          break;
-        }
-      case "ArrowDown" :
-        if(keyDebouncing[2] === 0) {
-          playSynth(2);
-          keyDebouncing[2] = 1;
-          break;
-        } else {
-          break;
-        }
-      case "ArrowUp" :
-        if(keyDebouncing[3] === 0) {
-          playSynth(3);
-          keyDebouncing[3] = 1;
-          break;
-        } else {
-          break;
-        }
-      case "Space" :
-        if(keyDebouncing[4] === 0) {
-          playSynth(4);
-          keyDebouncing[4] = 1;
-          break;
-        } else {
-          break;
-        }
-    }
-  }
-  
-  function handleKeyUp(e) {
-
     var key = e.code;
 
-    switch(key) {
-      case "ArrowLeft" :
-        stopSynth();
-        keyDebouncing[0] = 0;
-        break;
-      case "ArrowRight" :
-        stopSynth();
-        keyDebouncing[1] = 0;
-        break;
-      case "ArrowDown" :
-        stopSynth();
-        keyDebouncing[2] = 0;
-        break;
-      case "ArrowUp" :
-        stopSynth();
-        keyDebouncing[3] = 0;
-        break;
-      case "Space" :
-        stopSynth();
-        keyDebouncing[4] = 0;
-        break;
+    for(let i = 0; i < keys.length; i++){
+        if (key === keys[i]) {
+            if (keyDebouncing[i] === false) {
+                playSynth(i);
+                keyDebouncing[i] = true;
+            }
+        }
     }
-  }
+}
+
+function handleKeyUp(e) {
+var key = e.code;
+
+for(let i = 0; i < keys.length; i++){
+    if (key === keys[i]) {
+        stopSynth();
+        keyDebouncing[i] = false;
+    }
+}
+}
 
 
 
@@ -200,16 +150,14 @@ const visualisationColour = 255;
 const backgroundColour = 0;
 const backgroundAlpha = 30;
 
-  function setup() {
+  function setup() { // this is a P5.js function that sets up the canvas
     createCanvas(canvasSize, canvasSize);
     stroke(visualisationColour);
     noFill();
   }  
 
-  function draw() {
+  function draw() { // this is a P5.js function that animates at set frame rate
     background(backgroundColour, backgroundAlpha);
-
     let level = visualisationMeter.getValue();
-
     circle(canvasSize/2, canvasSize/2, level * (canvasSize * 2))
   }  
